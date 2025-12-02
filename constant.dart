de3c57f11691b1a42637a189955b643b369223db
Ritika Gupta <gritika859@gmail.com>
2025-12-02 12:21:05 +0530

    Add AppConstants class with configuration constants

---
+++ constant.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const eventsCollection = "events";
+  static const usersCollection = "users";
+
+  static const maxVideoSizeBytes = 5 * 1024 * 1024;
+  static const maxVideoDurationSeconds = 15;
+  static const maxImageSizeBytes = 300 * 1024;
+}
